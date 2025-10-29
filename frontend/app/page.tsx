256a696f9d5710596af3b40df8fdc61d21b3b988
kaketo0113 <m.kaketo113@gmail.com>
2025-10-29 11:35:14 +0900

    相対パスに変更

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
 import { DUMMY_POSTS } from '@/lib/dummy-data'; // (パスは適宜調整)
-import { PostCard } from '@/components/PostCard'; // (これから作るコンポーネント)
+import { PostCard } from '../components/PostCard'; // (これから作るコンポーネント)
 
 export default function Home() {
   return (
